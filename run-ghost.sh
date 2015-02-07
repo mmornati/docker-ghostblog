fb00e11835064399cbce4a5b5ad93deb2ba41dda
x-works <x-works@users.noreply.github.com>
2015-02-07 22:20:23 +0800

    Update run-ghost.sh

--- run-ghost.sh
+++ run-ghost.sh
@@ -44,10 +44,23 @@ if [[ -d "$OVERRIDE/$THEMES" ]]; then
   done
 fi
 
+# Set Config
+SERVER_PORT=80
+
 echo "=> Change config based on ENV parameters:"
 echo "========================================================================"
-echo "      WEB_URL:    $WEB_URL"
+echo "      WEB_URL:        $WEB_URL"
+echo "      DB_CLIENT:      $DB_CLIENT"
+echo "      SQLITE_PATH:    $DB_SQLITE_PATH"
+echo "      SERVER_HOST:    $SERVER_HOST"
+echo "      SERVER_PORT:    $SERVER_PORT"
 echo "========================================================================"
+sed "s/__WEB_URL__/$WEB_URL/" $OVERRIDE/$CONFIG > $OVERRIDE/$CONFIG
+sed "s/__DB_CLIENT__/$DB_CLIENT/" $OVERRIDE/$CONFIG > $OVERRIDE/$CONFIG
+sed "s/__DB_SQLITE_PATH__/$DB_SQLITE_PATH/" $OVERRIDE/$CONFIG > $OVERRIDE/$CONFIG
+sed "s/__SERVER_HOST__/$SERVER_HOST/" $OVERRIDE/$CONFIG > $OVERRIDE/$CONFIG
+sed "s/__SERVER_PORT__/$SERVER_PORT/" $OVERRIDE/$CONFIG > $OVERRIDE/$CONFIG
+
 
 # Start Ghost
 chown -R ghost:ghost /data /ghost /ghost-override
