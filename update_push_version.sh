17f88a3c4f571260ccc570ec9f23aa7534623d51
Marco Mornati <ilmorna@gmail.com>
2019-04-20 17:06:11 +0200

    fix: sed command into update version script. Preventing backup file

--- update_push_version.sh
+++ update_push_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$1 && \
-sed -i -e "s/^ARG GHOST_VERSION.*$/ARG GHOST_VERSION=\"$VERSION\"/" Dockerfile && \
+sed -i '' "s/^ARG GHOST_VERSION.*$/ARG GHOST_VERSION=\"$VERSION\"/" Dockerfile && \
 git add . && \
 git commit -m "Ghost updated to $VERSION version" && \
 git tag -a $VERSION -m "Ghost updated to $VERSION version" && \
